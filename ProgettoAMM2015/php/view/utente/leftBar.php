652cc7b893e66c0dce51ebac3a44dfde917e4d71
Federica Muceli <federica.muceli@hotmail.it>
2016-02-12 19:10:06 +0100

    modifica

--- ProgettoAMM2015/php/view/utente/leftBar.php
+++ ProgettoAMM2015/php/view/utente/leftBar.php
@@ -5,11 +5,11 @@ switch ($vd->getSottoPagina()) {
 case 'nuovoOrdine':
 if (isset ($_SESSION['newOrdine']))unset($_SESSION['newOrdine']);
 ?><ul>
-    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente<?= $vd->scriviToken('?')?>">Home</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente">Home</a></li>
     <li><a href="#segnalibro">Contatti</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo<?= $vd->scriviToken('?')?>">Dati personali</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini<?= $vd->scriviToken('?')?>">Ordini completati</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine<?= $vd->scriviToken('?')?>">Nuovo ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo">Dati personali</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini">Ordini completati</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine">Nuovo ordine</a></li>
 <p><br><br></p>
 </ul>
 <img title='culurgionis' alt="Culurgionis" src="../immagini/culurgionis3.jpeg" width="250" height="150"><?
@@ -18,23 +18,21 @@ case 'ordini':
 if (isset ($_SESSION['newOrdine']))unset($_SESSION['newOrdine']);
 ?>
 <ul>
-    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente<?= $vd->scriviToken('?')?>">Home</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente">Home</a></li>
     <li><a href="#segnalibro">Contatti</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo<?= $vd->scriviToken('?')?>">Dati personali</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini<?= $vd->scriviToken('?')?>">Ordini completati</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine<?= $vd->scriviToken('?')?>">Nuovo ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo">Dati personali</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini">Ordini completati</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine">Nuovo ordine</a></li>
 </ul>
 <?
 break;
-case 'riepilogo':
-if (isset ($_SESSION['newOrdine']))unset($_SESSION['newOrdine']);
-?>
+case 'riepilogo':?>
 <ul>
-    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente<?= $vd->scriviToken('?')?>">Home</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente">Home</a></li>
     <li><a href="#segnalibro">Contatti</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo<?= $vd->scriviToken('?')?>">Dati personali</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini<?= $vd->scriviToken('?')?>">Ordini completati</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine<?= $vd->scriviToken('?')?>">Nuovo ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo">Dati personali</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini">Ordini completati</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine">Nuovo ordine</a></li>
 </ul>
 <?
 break;
@@ -43,11 +41,11 @@ case 'profilo':
 if (isset ($_SESSION['newOrdine']))unset($_SESSION['newOrdine']);
 ?>
 <ul>
-    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente<?= $vd->scriviToken('?')?>">Home</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente">Home</a></li>
     <li><a href="#segnalibro">Contatti</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo<?= $vd->scriviToken('?')?>">Dati personali</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini<?= $vd->scriviToken('?')?>">Ordini completati</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine<?= $vd->scriviToken('?')?>">Nuovo ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo">Dati personali</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini">Ordini completati</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine">Nuovo ordine</a></li>
 </ul><?
 break;
 
@@ -55,11 +53,11 @@ case 'credenziali':
 if (isset ($_SESSION['newOrdine']))unset($_SESSION['newOrdine']);
 ?>
 <ul>
-    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente<?= $vd->scriviToken('?')?>">Home</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="index.php?page=utente">Home</a></li>
     <li><a href="#segnalibro">Contatti</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo<?= $vd->scriviToken('?')?>">Dati personali</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini<?= $vd->scriviToken('?')?>">Ordini completati</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine<?= $vd->scriviToken('?')?>">Nuovo ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo">Dati personali</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini">Ordini completati</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine">Nuovo ordine</a></li>
 </ul><?
 break;
 default:
