b34c42c53b8fa3c0a8ed7f24f031c5d832ef1198
Federica Muceli <federica.muceli@hotmail.it>
2016-02-12 19:28:26 +0100

    modifica

--- ProgettoAMM2015/php/view/utente/leftBar.php
+++ ProgettoAMM2015/php/view/utente/leftBar.php
@@ -9,9 +9,9 @@ case 'nuovoOrdine':
     <li class="<?= $vd->getSottoPagina() == 'profilo' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=profilo">Dati personali</a></li>
     <li class="<?= $vd->getSottoPagina() == 'ordini' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=ordini">Ordini completati</a></li>
     <li class="<?= $vd->getSottoPagina() == 'nuovoOrdine' ? 'current_page_item' : '' ?>"><a href="index.php?page=utente&subpage=nuovoOrdine">Nuovo ordine</a></li>
-<p><br><br></p>
+</p>
 </ul>
-<img title='culurgionis' alt="Culurgionis" src="../immagini/culurgionis3.jpeg" width="250" height="150"><?
+
 break;
 case 'ordini':
 if (isset ($_SESSION['newOrdine']))unset($_SESSION['newOrdine']);
